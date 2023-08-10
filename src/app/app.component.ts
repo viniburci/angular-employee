1239e678150885764c52ad522a216ff9e458dadb
Vinicius Burci <vpburci@gmail.com>
2023-08-10 05:20:50 -0300

    Include Bootstraps CSS and JS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+declare var jQuery: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-frontend';
+
+  constructor(){}
+
+  ngOnInit() {
+    jQuery("#myselector").style="display: none;";
+  }
 }
